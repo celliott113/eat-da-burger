09daa421ce0a53cd956c91fe9702730fae2b6159
Christopher Elliott <christopher.elliott8@gmail.com>
2017-11-10 08:42:38 -0600

    Testing another database for stability

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-};
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "jemw3s9cg2piczpr",
+  password: "ky005pyp3iqz8kl7",
+  database: "burgers_db"
+});
 
 // Make connection.
 connection.connect(function (err) {
   if (err) {
-    console.error("Error connecting: " + err.stack);
+    console.error("error connecting: " + err.stack);
     return;
   }
-  console.log("Connected as id: " + connection.threadId);
+  console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
