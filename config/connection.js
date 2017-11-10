986ec948d9ffc3c6e774d9a8d8f0a9e0f5f618f5
Christopher Elliott <christopher.elliott8@gmail.com>
2017-11-10 10:50:21 -0600

    Testing alternate JawsDB instance

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 var connection;
- 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-};
- 
- // var connection = mysql.createConnection({
+
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// };
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "yhrz9vns005e0734.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "bnqg80r3ry4be5nv",
+  password: "avp14ygcbmo2xsz7",
+  database: "s8olknqpg47vlnav"
+});
+
+// var connection = mysql.createConnection({
 //   port: 3306,
 //   host: "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 //   user: "jemw3s9cg2piczpr",
@@ -17,10 +25,10 @@ if (process.env.JAWSDB_URL) {
 // Make connection.
 connection.connect(function (err) {
   if (err) {
-   console.error("Error connecting: " + err.stack);
+    console.error("Error connecting: " + err.stack);
     return;
   }
- console.log("Connected as id: " + connection.threadId);
+  console.log("Connected as id: " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
