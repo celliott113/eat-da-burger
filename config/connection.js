92c59b5837711bb2f1dc31eb0299fa3b4f13bb3f
Christopher Elliott <christopher.elliott8@gmail.com>
2017-11-10 10:17:31 -0600

    Reverting back to original JawsDB to test stability.

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "jemw3s9cg2piczpr",
-  password: "ky005pyp3iqz8kl7",
-  database: "mzi0qo1smzmryxf6"
-});
+var connection;
+ 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+};
+ 
+ // var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   user: "jemw3s9cg2piczpr",
+//   password: "ky005pyp3iqz8kl7",
+//   database: "mzi0qo1smzmryxf6"
+// });
 
 // Make connection.
 connection.connect(function (err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+   console.error("Error connecting: " + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+ console.log("Connected as id: " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
