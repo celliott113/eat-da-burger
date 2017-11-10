3a1d9711424e0b75550e31f78e14572a41a5a609
Christopher Elliott <christopher.elliott8@gmail.com>
2017-11-10 08:18:14 -0600

    Commit #10

--- config/connection.js
+++ config/connection.js
@@ -9,10 +9,10 @@ if (process.env.JAWSDB_URL) {
 // Make connection.
 connection.connect(function (err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error("Error connecting: " + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log("Connected as id: " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
